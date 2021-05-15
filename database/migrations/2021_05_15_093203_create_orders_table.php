45c657a33629bbc66a5be41a95aafd6e6cc1a52f
BimaAdi <bimaadi419@gmail.com>
2021-05-15 16:42:07 +0700

    add table migrations

---
+++ database/migrations/2021_05_15_093203_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number');
+            $table->string('table_number');
+            $table->unsignedBigInteger('waiter_id');
+            $table->boolean('is_done')->default(false);
+            $table->timestamps();
+
+            $table->foreign('waiter_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2021_05_15_093805_create_order_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_menus', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('menu_id');
+            $table->integer('current_price');
+            $table->integer('qty');
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('menu_id')->references('id')->on('menus');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_menus');
+    }
+}
